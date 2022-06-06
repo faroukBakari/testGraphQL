f2b90dfc228d34aa7c4d22093c433c57dd8a624f
Farouk Bakari <baccari.farouk.pro@gmail.com>
2022-06-06 21:14:03 +0200

    feat: adding demo page

--- src/Client/webpack.config.ts
+++ src/Client/webpack.config.ts
@@ -1,9 +1,11 @@
 import path from "path";
-// import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-	entry: ["./src/Client/index.ts"],
+	entry: {
+		"index" : "./src/Client/index.ts",
+		"demo" : "./src/Client/demo.ts"
+	},
 	devtool: "source-map",
 	target: process.env.NODE_ENV === "production" ? "browserslist" : "web",
 	mode: process.env.NODE_ENV === "production" ? "production" : ("development" as "production" | "development" | "none" | undefined),
@@ -16,7 +18,6 @@ export default {
 	module: {
 		rules: [
 			{ test: /\.ts$/, exclude: /node_modules/, use: [ "babel-loader", 'ts-loader'] },
-			{ test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
 			{ test: /\.s?css$/, exclude: /node_modules/, use: ["style-loader", "css-loader", "postcss-loader", { loader: "sass-loader", options: { sourceMap: false } }] }, // sourceMap: false prevent style crashing when live-updating
 			{
 				test: /\.(ico|jpg|png)$/,
@@ -28,8 +29,8 @@ export default {
 	},
 
 	plugins: [
-		// new MiniCssExtractPlugin({ filename: 'style/[name].css'}),
-		new HtmlWebpackPlugin({ template: "./src/Client/index.html" }),
+		new HtmlWebpackPlugin({ filename: 'index.html', template: "./src/Client/index.html", chunks: ['index'] }),
+		new HtmlWebpackPlugin({ filename: 'demo.html', template: "./src/Client/demo.html", chunks: ['demo'] }),
 	],
 
 	output: {
